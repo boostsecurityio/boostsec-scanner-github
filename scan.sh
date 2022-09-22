9262d3c5e9e2c3f21623b17855624344677c24b6
Jonathan Serafini <jonathan@serafini.ca>
2022-09-22 10:13:53 -0400

    update github action for boost-scanner-cli

--- scan.sh
+++ scan.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -u
 
 export BOOST_TMP_DIR=${BOOST_TMP_DIR:-${WORKSPACE_TMP:-${TMPDIR:-/tmp}}}
-export BOOST_EXE=${BOOST_EXE:-${BOOST_TMP_DIR}/boost/cli/latest/boost.sh}
+export BOOST_EXE=${BOOST_EXE:-${BOOST_TMP_DIR}/boost-cli/latest}
 
 
 log.info ()
@@ -24,11 +24,8 @@ init.config ()
 
   export BOOST_CLI_URL=${BOOST_CLI_URL:-https://assets.build.boostsecurity.io}
          BOOST_CLI_URL=${BOOST_CLI_URL%*/}
-  export BOOST_DOWNLOAD_URL=${BOOST_DOWNLOAD_URL:-${BOOST_CLI_URL}/boost/get-boost-cli}
-  export BOOST_EXEC_COMMAND=${INPUT_EXEC_COMMAND:-}
-
+  export BOOST_DOWNLOAD_URL=${BOOST_DOWNLOAD_URL:-${BOOST_CLI_URL}/boost-cli/get-boost-cli}
   export BOOST_FORCE_COLORS="true"
-  export DOCKER_COPY_REQUIRED=false
 }
 
 init.cli ()
@@ -41,41 +38,13 @@ init.cli ()
   curl --silent "${BOOST_DOWNLOAD_URL}" | bash
 }
 
-main.exec ()
-{
-  init.config
-  init.cli
-
-  if [ -z "${BOOST_EXEC_COMMAND:-}" ]; then
-    log.error "the 'exec_command' option must be defined when in exec mode"
-    exit 1
-  fi
-
-  if [ -z "${BOOST_STEP_NAME:-}" ]; then
-    log.error "the 'step_name' option must be defined in exec mode"
-    exit 1
-  fi
-
-  exec ${BOOST_EXE} scan exec ${BOOST_CLI_ARGUMENTS:-} --command "${BOOST_EXEC_COMMAND}"
-}
-
 main.scan ()
 {
   init.config
   init.cli
 
-  if [ -n "${BOOST_EXEC_COMMAND:-}" ]; then
-    log.error "the 'exec_command' option must only be defined in exec mode"
-    exit 1
-  fi
-
+  # shellcheck disable=SC2086
   exec ${BOOST_EXE} scan run ${BOOST_CLI_ARGUMENTS:-}
 }
 
-case "${INPUT_ACTION:-scan}" in
-  exec)     main.exec ;;
-  scan)     main.scan ;;
-  *)        log.error "invalid action ${INPUT_ACTION}"
-            exit 1
-            ;;
-esac
+main.scan
